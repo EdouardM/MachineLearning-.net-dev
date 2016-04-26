e7c00c4aae6d07e393d49c87669b0d47b9d9f555
Edouard Moureaux <edouard.moureaux@gmail.com>
2016-04-26 22:46:47 +0200

    Chapter1: Digit Recognizer

---
+++ MachineLearning - .Net Programmer/DigitRecognizer.fs
@@ -0,0 +1,48 @@
+﻿namespace MachineLearning.DigitRecognizer
+
+///1-nearest neighbour
+module Model =
+
+    type Observation = { Label: string; Pixels: int[] }
+    type Distance = int [] -> int [] -> int
+
+    let toObservation (csvData: string) =
+        let columns = csvData.Split [| ',' |]
+        let label = columns.[0]
+        let pixels = columns.[1..] |> Array.map int
+        {Label = label; Pixels = pixels }
+
+    let manathanDistance pixels1 pixels2 = 
+        Array.zip pixels1 pixels2
+        |> Array.map(fun (x,y) -> abs (x - y))
+        |> Array.sum
+
+    let euclideanDistance pixels1 pixels2 =
+        Array.zip pixels1 pixels2
+        |> Array.map(fun (x,y) -> pown (x - y) 2)
+        |> Array.sum
+        //|> sqrt We drop sqrt for faster calculation
+    
+    ///Search in the training set the element closest to the image to classify
+    ///1-nearest neighbour
+    let train (dist:Distance) (trainingSet: Observation []) =
+        let classify (pixels: int []) =
+            trainingSet
+            |> Array.minBy (fun x -> dist x.Pixels pixels )
+            |> (fun x -> x.Label)
+        classify 
+
+    ///Validate model based on accuracy
+    let evaluate classifier data =
+        data
+        |> Array.averageBy(fun x -> if classifier x.Pixels = x.Label then 1. else 0.)
+        |> printfn "Correct: %.3f"
+    
+module Utils = 
+    open System.IO
+    open Model
+
+    let reader path =
+        let data = File.ReadAllLines path
+        data.[1..]
+        |> Array.map toObservation 
---
+++ MachineLearning - .Net Programmer/DigitRecognizer.fsx
@@ -0,0 +1,28 @@
+﻿#load "DigitRecognizer.fs"
+
+open MachineLearning.DigitRecognizer.Utils
+open MachineLearning.DigitRecognizer.Model
+
+let trainingfilename = "trainingsample.csv"
+let trainingfilepath =  __SOURCE_DIRECTORY__ + @"..\..\data\" + trainingfilename
+
+let trainingdata = reader trainingfilepath
+let test = trainingdata.[100].Label
+
+
+///Takes array of pixels and returns predicted label
+let manathanClassifier = train manathanDistance trainingdata 
+let euclideanClassifier = train euclideanDistance trainingdata 
+
+
+let validationfilename = "validationsample.csv"
+let validationfilepath = __SOURCE_DIRECTORY__ + @"..\..\data\" + validationfilename
+
+let validationData = reader validationfilepath
+
+printfn "Manathan:" 
+evaluate manathanClassifier validationData
+
+printfn "Euclidean:" 
+evaluate euclideanClassifier validationData
+
